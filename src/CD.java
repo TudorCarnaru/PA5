87f2eb5acd2225c982243bbc42de7db5772c771e
genius252 <tud_underworld@yahoo.com>
2016-04-01 21:08:40 +0300

    Asta e tema cica

--- src/CD.java
+++ src/CD.java
@@ -1,5 +1,27 @@
 /**
  * Created by genius on 4/1/2016.
  */
-public class CD {
+import java.io.File;
+
+public class CD extends Command {
+    File f;
+    public CD(){
+    }
+
+    public File setFile(Command c, File f){
+        if (c.getParamSize()!=0) {
+            f = new File(c.getParamIndex(0));
+            if (!f.exists())
+                f.mkdir();
+        }
+        else
+        {
+            String s = f.getAbsolutePath();
+            String goBack;
+            goBack=s.substring(0,s.lastIndexOf('\\'));
+            f = new File(goBack);
+        }
+        return f;
+    }
 }
+
